0c6d77bf75785043c549bec5ecf1018765dfb2ca
Sandro Shavdatuashvili <sandro.shav@gmail.com>
2022-11-10 23:52:55 +0400

    learning before props

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="main-class">
+<!--    directives-->
     <h1 v-if="render===true">Shopping mall is open</h1>
     <h1 v-else-if="render===false">Sorry, we're closed</h1>
     <h1 v-else>Something bad happened to website</h1>
@@ -9,24 +10,29 @@
     <br>
     <h3 :class="{'dev-text' : developerClass}">Developers:</h3>
     <p v-for="(item,index) in developersArray" :key="index">{{ item }}</p>
+<!--    -->
     <div v-html="editor"></div>
+<!--    -->
+<!--    interacted input-->
     <label for="text">Enter:</label>
     <input type="text" v-model="textValue">
-    <br>
     {{ textValue }}
+<!--    -->
     <div class="box" @click="clickFunction">
       {{ getData }}
     </div>
-
+<!--    button increase-decrease-->
     <button @click="numberVariable--">-</button>
     {{ numberVariable }}
     <button @click="numberVariable++">+</button>
     <p>You want {{getData}} piece</p>
+<!--    -->
   </div>
 </template>
 
 <script>
 export default {
+  // data is for variables we use in html, variables like Array, String, div block etc.
   data() {
     return {
       numberVariable: 0,
@@ -39,22 +45,26 @@ export default {
       developersArray: ['Sandro Shavdatuashvili', 'Some other dev']
     }
   },
+  // methods is for functions like click, for example: @click='changeColor'
   methods: {
     clickFunction() {
       console.log(this.dataArray)
     }
   },
+  // is called after page will be uploaded in browser
   mounted() {
     this.clickFunction()
   },
   created() {
     console.log(this.render)
   },
+  // computed is a reactive data
   computed: {
     getData() {
-      return this.numberVariable //this is reactive data
+      return this.numberVariable
     }
   },
+  //watch is a like a watcher to a specific paragraph or a div, we can make
   watch: {
     numberVariable: {
       immediate: true,
