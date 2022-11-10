cb1aa235057e91b14637876eecaffa2056490429
Sandro Shavdatuashvili <sandro.shav@gmail.com>
2022-11-10 21:28:45 +0400

    Upload vue project

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>This is Header</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+
+<style scoped>
+h1{
+  font-size: 56px;
+  color: orange;
+}
+</style>
